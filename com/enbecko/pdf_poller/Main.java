b4538049dee910b88eba59e4679179029231fc2b
Niclas Becker <enbecko1996@googlemail.com>
2016-12-04 00:14:26 +0100

    some stupid changes

--- com/enbecko/pdf_poller/Main.java
+++ com/enbecko/pdf_poller/Main.java
@@ -4,9 +4,10 @@ package com.enbecko.pdf_poller;
  * Created by enbec on 04.12.2016.
  */
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("hello world and listen to my music xD");
+        if(2 > 1)
+            System.out.println("the wolf hunts for its prey");
+        else
+            System.out.println("ryushiwagatekiukurei");
     }
-
 }
