4b37d7c18eef6ec09cb9b8f2d26c147e04664d02
Niclas Becker <enbecko1996@googlemail.com>
2016-12-04 00:05:09 +0100

    added Main class

---
+++ com/enbecko/pdf_poller/Main.java
@@ -0,0 +1,12 @@
+package com.enbecko.pdf_poller;
+
+/**
+ * Created by enbec on 04.12.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
