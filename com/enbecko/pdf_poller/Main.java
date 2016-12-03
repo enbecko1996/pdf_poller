546c7ca2b6744ddf2b54bb01ef9f1fbf3d6b270d
Niclas Becker <enbecko1996@googlemail.com>
2016-12-04 00:08:15 +0100

    changed print txt

--- com/enbecko/pdf_poller/Main.java
+++ com/enbecko/pdf_poller/Main.java
@@ -6,7 +6,7 @@ package com.enbecko.pdf_poller;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world and listen to my music xD");
     }
 
 }
